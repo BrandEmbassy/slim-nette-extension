876feddb314402ee795f3e8d99154e6302529d09
dominikvoda <d.voda94@gmail.com>
2020-10-24 18:35:41 +0200

    @ fix withJson types

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -4,9 +4,6 @@ namespace BrandEmbassy\Slim\Response;
 
 use Slim\Http\Response as SlimResponse;
 
-/**
- * @method static withJson(array $data, ?int $status = null, int $encodingOptions = 0)
- */
-final class Response extends SlimResponse implements ResponseInterface
+class Response extends SlimResponse implements ResponseInterface
 {
 }
--- src/Response/ResponseInterface.php
+++ src/Response/ResponseInterface.php
@@ -3,13 +3,14 @@
 namespace BrandEmbassy\Slim\Response;
 
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
+use stdClass;
 
 interface ResponseInterface extends PsrResponseInterface
 {
     /**
-     * @param mixed[] $data
+     * @param mixed[]|stdClass $data
      *
      * @return static
      */
-    public function withJson(array $data, ?int $status = null, int $encodingOptions = 0);
+    public function withJson($data, ?int $status = null, int $encodingOptions = 0);
 }
