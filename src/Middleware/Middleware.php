7aa2c52fb1cf3ecaabe1aa37d1719da314a514d2
petrmatulik <matulik@gmail.com>
2025-12-03 14:50:01 +0100

    add Middleware from version 5.10

---
+++ src/Middleware/Middleware.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Slim\Middleware;
+
+use BrandEmbassy\Slim\Request\RequestInterface;
+use BrandEmbassy\Slim\Response\ResponseInterface;
+
+interface Middleware
+{
+    public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface;
+}
