2766067de425e56c351219dd3446d496086fc4f9
petrmatulik <matulik@gmail.com>
2025-12-09 19:04:22 +0100

    Add missing exceptions

---
+++ src/Request/RequestFieldMissingException.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Slim\Request;
+
+use function sprintf;
+
+/**
+ * @final
+ */
+class RequestFieldMissingException extends RequestException
+{
+    public static function create(string $fieldName): self
+    {
+        return new self(sprintf('Field "%s" is missing in request body', $fieldName));
+    }
+}
